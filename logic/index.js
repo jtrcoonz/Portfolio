b67bc10b4f5f6c0ee515019a63d5d48c3280846a
Jacob Coons <jacobtcoons@gmail.com>
2019-12-14 19:41:36 -0800

    style almost finished

--- logic/index.js
+++ logic/index.js
@@ -1,7 +1,9 @@
 $(".drop-menu").click(() => {
     $(".drop-menu-opened").fadeIn();
+    $(".black-box").fadeIn();
 });
 
 $(".close-drop-menu").click(() => {
     $(".drop-menu-opened").fadeOut();
+    $(".black-box").fadeOut();
 });
