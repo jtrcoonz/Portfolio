22814b1ffecec25fee50cb67b258443a8170762d
Jacob Coons <jacobtcoons@gmail.com>
2020-01-17 11:13:06 -0800

    created logic and styling to turn the site title into a link to home for additional navigation

--- logic/index.js
+++ logic/index.js
@@ -7,3 +7,7 @@ $(".close-drop-menu").click(() => {
     $(".drop-menu-opened").fadeOut();
     $(".black-box").fadeOut();
 });
+
+$(".site-title").click(() => {
+    window.location.href = "./index.html"
+})
